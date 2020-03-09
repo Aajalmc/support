c55222a1838c84c6019983ed40c465f6c3637d5f
M.C <amc@ptc.com>
2020-03-09 10:45:45 +0530

    bold print added

--- src/test/java/support_automation/supportpdfs.java
+++ src/test/java/support_automation/supportpdfs.java
@@ -263,7 +263,14 @@ for (String handle : handles1) {
     //Screenshot fpScreenshot1 = new AShot().shootingStrategy(ShootingStrategies.scaling(1)).takeScreenshot(driver);
     //ImageIO.write(fpScreenshot1.getImage(),"PNG",new File("C:\\Users\\ntiwari\\Desktop\\Selenium Screenshot\\Screenshot2.png"));
 	System.out.println("Screenshot taken for detail page of search result");
-	System.out.println("Screenshots are availabe here : \\\\ppumsv-Win16Jen\\\\Selenium_Screenshot");
+	
+	//Click on English PDF icon
+	driver.findElement(By.xpath("//span[contains(text(),'English')]")).click();
+	Thread.sleep(5000);
+	Screenshot fpScreenshot2 = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(2)).takeScreenshot(driver);
+    ImageIO.write(fpScreenshot2.getImage(),"PNG",new File("D:\\Selenium_Screenshot\\Screenshot3.png"));
+    
+	System.out.println("Screenshots are availabe here : \\\\ppumsv-Win16Jen\\Selenium_Screenshot");
   
 }
 
