266d980b406ac1b34073f48fa6ea00f0f38532aa
Vasanth <vpotturu@ptc.com>
2020-03-17 16:10:04 +0530

    Reduce & increase size for pdf capture

--- src/test/java/support_automation/supportpdfs.java
+++ src/test/java/support_automation/supportpdfs.java
@@ -275,7 +275,7 @@ for (String handle : handles1) {
 	//Click on English PDF icon
 	driver.findElement(By.xpath("//span[contains(text(),'English')]")).click();
 	Thread.sleep(12000);
-	//driver.manage().window().setSize(new org.openqa.selenium.Dimension(1080,1920));
+	driver.manage().window().setSize(new org.openqa.selenium.Dimension(1080,1920));
 	Thread.sleep(1000);
 	//code for screenshot
 	Screenshot fpScreenshot2 = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000)).takeScreenshot(driver);
